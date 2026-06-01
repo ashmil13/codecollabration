a13482df8359f7a16aae9dd81dbef604ff58b223
ashmil13 <ashmil13june@gmail.com>
2026-06-01 23:29:47 +0530

    thirdcommit

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,9 @@ app.use(cors({
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-
+app.get("/", (req, res) => {
+  res.json({ message: "Projects API" });
+});
 app.use('/api', userRouter);
 app.use('/api', projectRouter);
 app.use('/api', versionRouter);
